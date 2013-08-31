cd73604e989a47a8a4eb4b3ef091850ca137dd4f
Lee Krueger <leekdpc@yahoo.com>
2013-08-31 12:47:45 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MHartlTutorial::Application.config.secret_key_base = '0bd4f67104ee97b9b4f85fa311d30f804d9b78ea5277fcd4b800585f4ef62f24a7ad5d2798e4ac9f904b4dcae21a6db83453f270c7ae084444b0248bc5749c57'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MHartlTutorial::Application.config.session_store :cookie_store, key: '_MHartl_tutorial_session'
